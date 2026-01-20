6e22e2cbca781a45682779fdedb71183f65d597b
Muhammad Maulana Agung <muhammadmaulanaagung67@gmail.com>
2026-01-20 14:27:06 +0700

    menambahkan fungsi waktu secara realtime

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const doneList = document.getElementById('done-list');
 const clearButton = document.getElementById('delete-all-btn');
 const displayDate = document.getElementById('display-date');
 
+// Menampilkan Waktu secara realtime
+function updateWaktu() {
+    const sekarang = new Date();
+
+    const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+    const formattedDate = sekarang.toLocaleDateString('en-US', options);
+
+    displayDate.textContent = formattedDate;
+}
+setInterval(updateWaktu, 1000);
+
+updateWaktu();
+
+
