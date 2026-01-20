f21b17af516383d58e6f619f3312cbe045eb8b6e
Muhammad Maulana Agung <muhammadmaulanaagung67@gmail.com>
2026-01-20 14:19:06 +0700

    inisialisasi seleksi elemen DOM di script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('todo-form');
+const input = document.getElementById('todo-input');
+const priority = document.getElementById('priority-level');
+const todoList = document.getElementById('todo-list');
+const doneList = document.getElementById('done-list');
+const clearButton = document.getElementById('delete-all-btn');
+const displayDate = document.getElementById('display-date');
+
